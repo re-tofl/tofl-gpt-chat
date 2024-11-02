27e84338bdb86630a6f867ea61e6fb50d7e888d3
darleet <darleet@mail.ru>
2024-11-03 01:41:27 +0300

    migrations fix: fix migration

--- db/migrations/20241026133130_alter_user_add_indices_change_state_type.sql
+++ db/migrations/20241026133130_alter_user_add_indices_change_state_type.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE chat.user ALTER COLUMN state TYPE SMALLINT;
-
 CREATE UNIQUE INDEX user_chat_id_idx ON chat.user (chat_id);
+ALTER TABLE chat.user DROP COLUMN state;
+ALTER TABLE chat.user ADD COLUMN state SMALLINT;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+ALTER TABLE chat.user ADD COLUMN state TEXT;
+ALTER TABLE chat.user DROP COLUMN state;
 DROP INDEX user_chat_id_idx;
-
-ALTER TABLE chat.user ALTER COLUMN state TYPE TEXT;
 -- +goose StatementEnd
