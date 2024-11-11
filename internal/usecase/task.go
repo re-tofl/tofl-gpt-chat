8cd18bc8a4a4db93cd37a82ea6fef3eea786c3d7
Artem <List.kedra.79@gmail.com>
2024-11-12 00:45:23 +0300

    Clean code and architect

--- internal/usecase/task.go
+++ internal/usecase/task.go
@@ -3,20 +3,18 @@ package usecase
 import (
 	"context"
 
-	"go.uber.org/zap"
-
 	"github.com/re-tofl/tofl-gpt-chat/internal/domain"
 )
 
 type TaskStore interface {
 	Translate(message *domain.Message) *domain.Message
-	//SendToOpenAi(message *tgbotapi.Message, files []domain.File) domain.OpenAiResponse
+	Search(userMessage *domain.Message) *domain.Message
 }
 
-func Translate(ctx context.Context, ts TaskStore, logger *zap.SugaredLogger, message *domain.Message) *domain.Message {
+func Translate(ctx context.Context, ts TaskStore, message *domain.Message) *domain.Message {
 	return ts.Translate(message)
 }
 
-/*func ResolveProblemOnImage(ctx context.Context, ts TaskStore, logger *zap.SugaredLogger, message *tgbotapi.Message, files []domain.File) {
-	ts.SendToOpenAi(message, files)
-}*/
+func Search(ctx context.Context, ts TaskStore, message *domain.Message) *domain.Message {
+	return ts.Search(message)
+}
