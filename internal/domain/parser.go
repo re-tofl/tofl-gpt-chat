51be0a3130dcec5c6e5a9490356c0da360158924
darleet <darleet@mail.ru>
2024-11-23 14:25:56 +0300

    domain feat: add formal and parser request and response

--- internal/domain/parser.go
+++ internal/domain/parser.go
@@ -4,34 +4,3 @@ type ParserRequest struct {
 	TRS            string `json:"TRS"`
 	Interpretation string `json:"Interpretation"`
 }
-
-type ParserErrorResponse struct {
-	ErrorTrs            []string `json:"error_trs"`
-	ErrorInterpretation []string `json:"error_interpretation"`
-}
-
-type ParserResponse struct {
-	JsonTRS []struct {
-		Left struct {
-			Value  string `json:"value"`
-			Childs []struct {
-				Value  string        `json:"value"`
-				Childs []interface{} `json:"childs"`
-			} `json:"childs"`
-		} `json:"left"`
-		Right struct {
-			Value  string `json:"value"`
-			Childs []struct {
-				Value  string        `json:"value"`
-				Childs []interface{} `json:"childs"`
-			} `json:"childs"`
-		} `json:"right"`
-	} `json:"json_TRS"`
-	JsonInterpret struct {
-		Functions []struct {
-			Name       string   `json:"name"`
-			Variables  []string `json:"variables"`
-			Expression string   `json:"expression"`
-		} `json:"functions"`
-	} `json:"json_interpret"`
-}
