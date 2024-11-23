d91ed0aa53ef69206a7406810f63a8d32ded92d2
Ivan Pavlov <darleet@mail.ru>
2024-11-23 17:46:15 +0300

    fix problem handling

--- internal/delivery/telegram/task.go
+++ internal/delivery/telegram/task.go
@@ -13,7 +13,7 @@ import (
 
 // TODO: тут я уже решил забить на чистую архитектуру, потому что под нее надо рефакторить весь проект
 
-func (h *Handler) requestParser(ctx context.Context, message *tgbotapi.Message) (*http.Response, error) {
+func (h *Handler) requestParser(ctx context.Context, message *tgbotapi.Message) (*domain.ParserResponse, error) {
 	jsonStr := message.Text
 	req, err := http.NewRequestWithContext(ctx, "POST", h.cfg.ParserURL+"/parse", bytes.NewBuffer([]byte(jsonStr)))
 	if err != nil {
@@ -38,40 +38,58 @@ func (h *Handler) requestParser(ctx context.Context, message *tgbotapi.Message) 
 	}()
 
 	if resp.StatusCode != http.StatusOK {
-		h.log.Errorw("resp.StatusCode", zap.Error(err))
-		h.Send(tgbotapi.NewMessage(message.Chat.ID, "Произошла ошибка"))
+		h.log.Errorw("resp.StatusCode", "statusCode", resp.StatusCode, zap.Error(err))
+		if resp.StatusCode != http.StatusBadRequest {
+			h.Send(tgbotapi.NewMessage(message.Chat.ID, "Произошла ошибка"))
+			return nil, fmt.Errorf("resp.StatusCode: %w", err)
+		}
+
+		var data domain.ParserErrorResponse
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		if err != nil {
+			h.log.Errorw("json.Unmarshal", zap.Error(err))
+			h.Send(tgbotapi.NewMessage(message.Chat.ID, "Произошла ошибка"))
+			return nil, fmt.Errorf("json.Unmarshal: %w", err)
+		}
+
+		if data.ErrorTrs != nil {
+			h.log.Errorw("data.ErrorTrs", zap.Error(err))
+			h.Send(tgbotapi.NewMessage(message.Chat.ID, "Ошибки TRS"))
+			for _, err := range data.ErrorTrs {
+				h.Send(tgbotapi.NewMessage(message.Chat.ID, err))
+			}
+		}
+
+		if data.ErrorInterpretation != nil {
+			h.log.Errorw("data.ErrorInterpretation", zap.Error(err))
+			h.Send(tgbotapi.NewMessage(message.Chat.ID, "Ошибки интерпретации"))
+			for _, err := range data.ErrorInterpretation {
+				h.Send(tgbotapi.NewMessage(message.Chat.ID, err))
+			}
+		}
+
 		return nil, fmt.Errorf("resp.StatusCode: %w", err)
 	}
 
-	var data domain.ParserResponse
-	err = json.NewDecoder(resp.Body).Decode(&data)
+	var parserResp domain.ParserResponse
+	err = json.NewDecoder(resp.Body).Decode(&parserResp)
 	if err != nil {
 		h.log.Errorw("json.Unmarshal", zap.Error(err))
 		h.Send(tgbotapi.NewMessage(message.Chat.ID, "Произошла ошибка"))
 		return nil, fmt.Errorf("json.Unmarshal: %w", err)
 	}
 
-	if data.ErrorTrs != nil {
-		h.log.Errorw("data.ErrorTrs", zap.Error(err))
-		h.Send(tgbotapi.NewMessage(message.Chat.ID, "Ошибки TRS"))
-		for _, err := range data.ErrorTrs {
-			h.Send(tgbotapi.NewMessage(message.Chat.ID, err))
-		}
-	}
-
-	if data.ErrorInterpretation != nil {
-		h.log.Errorw("data.ErrorInterpretation", zap.Error(err))
-		h.Send(tgbotapi.NewMessage(message.Chat.ID, "Ошибки интерпретации"))
-		for _, err := range data.ErrorInterpretation {
-			h.Send(tgbotapi.NewMessage(message.Chat.ID, err))
-		}
-	}
-
-	return resp, nil
+	return &parserResp, nil
 }
 
-func (h *Handler) requestFormal(ctx context.Context, message *tgbotapi.Message, resp *http.Response) error {
-	req, err := http.NewRequestWithContext(ctx, "POST", h.cfg.FormalURL+"/data", resp.Body)
+func (h *Handler) requestFormal(ctx context.Context, message *tgbotapi.Message, parserResponse *domain.ParserResponse) error {
+	b, err := json.Marshal(parserResponse)
+	if err != nil {
+		h.log.Errorw("json.Marshal", zap.Error(err))
+		return fmt.Errorf("json.Marshal: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", h.cfg.FormalURL+"/data", bytes.NewBuffer(b))
 	if err != nil {
 		h.log.Errorw("http.NewRequest", zap.Error(err))
 		return fmt.Errorf("http.NewRequest: %w", err)
@@ -80,7 +98,7 @@ func (h *Handler) requestFormal(ctx context.Context, message *tgbotapi.Message, 
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
-	resp, err = client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		h.log.Errorw("client.Do", zap.Error(err))
 		return fmt.Errorf("client.Do: %w", err)
@@ -93,7 +111,7 @@ func (h *Handler) requestFormal(ctx context.Context, message *tgbotapi.Message, 
 	}()
 
 	if resp.StatusCode != http.StatusOK {
-		h.log.Errorw("resp.StatusCode", zap.Error(err))
+		h.log.Errorw("resp.StatusCode", "statusCode", resp.StatusCode, zap.Error(err))
 		return fmt.Errorf("resp.StatusCode: %w", err)
 	}
 
