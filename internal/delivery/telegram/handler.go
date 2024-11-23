88643a57ea2e94d4ba1b2c5428d28d511d58e75d
Ivan Pavlov <darleet@mail.ru>
2024-11-23 15:48:19 +0300

    app ref: refactor code

--- internal/delivery/telegram/handler.go
+++ internal/delivery/telegram/handler.go
@@ -3,29 +3,43 @@ package telegram
 import (
 	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"go.uber.org/zap"
-
 	"github.com/re-tofl/tofl-gpt-chat/internal/bootstrap"
-	task2 "github.com/re-tofl/tofl-gpt-chat/internal/delivery/openai"
-	"github.com/re-tofl/tofl-gpt-chat/internal/delivery/task"
+	"go.uber.org/zap"
 )
 
-type Handler struct {
-	cfg         *bootstrap.Config
-	log         *zap.SugaredLogger
-	bot         *tgbotapi.BotAPI
-	taskHandler *task.THandler
-	openHandler *task2.OpenHandler
+type OpenAiUsecase interface {
+	SaveMedia(ctx context.Context, message *tgbotapi.Message, bot *tgbotapi.BotAPI) string
+	SendToGpt(ctx context.Context, message *tgbotapi.Message) string
 }
 
-func NewHandler(cfg *bootstrap.Config, log *zap.SugaredLogger, t *task.THandler, o *task2.OpenHandler) *Handler {
+type SpeechUsecase interface {
+	ConvertSpeechToText(ctx context.Context, filePath string) string
+}
+
+type Handler struct {
+	cfg        *bootstrap.Config
+	log        *zap.SugaredLogger
+	bot        *tgbotapi.BotAPI
+	openAiUC   OpenAiUsecase
+	speechUC   SpeechUsecase
+	mu         sync.Mutex
+	userStates map[int64]string
+}
+
+func NewHandler(cfg *bootstrap.Config, log *zap.SugaredLogger,
+	o OpenAiUsecase, s SpeechUsecase) *Handler {
 	return &Handler{
-		cfg:         cfg,
-		log:         log,
-		taskHandler: t,
-		openHandler: o,
+		cfg:      cfg,
+		log:      log,
+		openAiUC: o,
+		speechUC: s,
 	}
 }
 
@@ -54,6 +68,7 @@ func (h *Handler) Listen(ctx context.Context) error {
 			bot.StopReceivingUpdates()
 			return nil
 		case update := <-updates:
+			// TODO: сделать многопоточным
 			h.processUpdate(ctx, &update)
 		}
 	}
@@ -92,20 +107,99 @@ func (h *Handler) processUpdate(ctx context.Context, update *tgbotapi.Update) {
 		return
 	}
 
+	if update.Message.Voice != nil {
+		h.handleVoice(ctx, update.Message, h.bot)
+	}
+
+	fmt.Println(update.Message)
+
 	if update.Message.IsCommand() {
 		h.processCommand(ctx, update.Message)
+	} else {
+		h.handleMessage(ctx, update.Message)
 	}
+
 }
 
 func (h *Handler) handleProblemWithImages(ctx context.Context, message *tgbotapi.Message) {
-	gptFileResponse := h.openHandler.SaveMediaAndSendToAi(ctx, message, h.bot)
-	fmt.Println(gptFileResponse)
+	gptFileResponse := h.openAiUC.SaveMedia(ctx, message, h.bot)
+	reply := tgbotapi.NewMessage(message.Chat.ID, gptFileResponse)
+	h.Send(reply)
+}
+
+func (h *Handler) handleVoice(ctx context.Context, message *tgbotapi.Message, bot *tgbotapi.BotAPI) {
+	fmt.Println("here")
+	fileId := message.Voice.FileID
+	file, err := bot.GetFile(tgbotapi.FileConfig{FileID: fileId})
+	if err != nil {
+		h.log.Error(err)
+	}
+
+	filePath, err := h.SaveAndDownloadVoice(file.FilePath, file.FileID)
+	textFromSpeech := h.speechUC.ConvertSpeechToText(ctx, filePath)
+
+	reply := tgbotapi.NewMessage(message.Chat.ID, textFromSpeech)
+	h.Send(reply)
+}
+
+func (h *Handler) SaveAndDownloadVoice(tgFilePath string, fileName string) (string, error) {
+	fileURL := fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", h.cfg.TGBotToken, tgFilePath)
+
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		return "", fmt.Errorf("ошибка при загрузке файла: %w", err)
+	}
+	defer resp.Body.Close()
+
+	filePath := filepath.Join("upload/voices", fileName+".ogg")
+	outFile, err := os.Create(filePath)
+	if err != nil {
+		return "", fmt.Errorf("ошибка при создании файла: %w", err)
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("ошибка при сохранении файла: %w", err)
+	}
+
+	return filePath, nil
+}
+func (h *Handler) handleGptTextMessage(ctx context.Context, message *tgbotapi.Message) {
+	gptResponse := h.openAiUC.SendToGpt(ctx, message)
+	reply := tgbotapi.NewMessage(message.Chat.ID, gptResponse)
+	h.Send(reply)
+}
+
+func (h *Handler) handleMessage(ctx context.Context, message *tgbotapi.Message) {
+	reply := tgbotapi.NewMessage(message.Chat.ID, "")
+
+	if state, exists := userStates[message.Chat.ID]; exists {
+		switch state {
+		case "awaiting_gpt_question":
+			question := message.Text
+			reply.Text = fmt.Sprintf("Ты задал вопрос: %s. Отправляю в GPT...", question)
+			h.Send(reply)
+			h.handleGptTextMessage(ctx, message)
+
+			delete(userStates, message.Chat.ID)
+		default:
+			reply.Text = "Неизвестное состояние."
+		}
+		return
+	}
+
+	h.processCommand(ctx, message)
 }
 
 func (h *Handler) processCommand(ctx context.Context, message *tgbotapi.Message) {
 	reply := tgbotapi.NewMessage(message.Chat.ID, "")
 
 	switch message.Command() {
+	case "gpt":
+		reply.Text = "Введи вопрос к gpt:"
+		userStates[message.Chat.ID] = "awaiting_gpt_question"
+		h.Send(reply)
 	case "theory":
 		reply.Text = "Ответ на теорию"
 		h.Send(reply)
@@ -115,7 +209,6 @@ func (h *Handler) processCommand(ctx context.Context, message *tgbotapi.Message)
 	case "imageProblem":
 		reply.Text = "Ответ на задачу с фотографиями"
 		h.handleProblemWithImages(ctx, message)
-
 		h.Send(reply)
 	case "developers":
 		reply.Text = "тут будут контакты разработчиков"
@@ -125,3 +218,5 @@ func (h *Handler) processCommand(ctx context.Context, message *tgbotapi.Message)
 		h.Send(reply)
 	}
 }
+
+var userStates = make(map[int64]string)
--- internal/delivery/telegram/task.go
+++ internal/delivery/telegram/task.go
@@ -1,9 +1,107 @@
 package telegram
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/re-tofl/tofl-gpt-chat/internal/domain"
+	"go.uber.org/zap"
+	"net/http"
+)
 
-func (h *Handler) Problem(ctx context.Context) error {
-	panic("implement me")
+// TODO: тут я уже решил забить на чистую архитектуру, потому что под нее надо рефакторить весь проект
+
+func (h *Handler) requestParser(ctx context.Context, message *tgbotapi.Message) (*http.Response, error) {
+	jsonStr := message.Text
+	req, err := http.NewRequestWithContext(ctx, "POST", h.cfg.ParserURL+"/parse", bytes.NewBuffer([]byte(jsonStr)))
+	if err != nil {
+		h.log.Errorw("http.NewRequest", zap.Error(err))
+		return nil, fmt.Errorf("http.NewRequest: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		h.log.Errorw("client.Do", zap.Error(err))
+		h.Send(tgbotapi.NewMessage(message.Chat.ID, "Произошла ошибка"))
+		return nil, fmt.Errorf("client.Do: %w", err)
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			h.log.Errorw("resp.Body.Close", zap.Error(err))
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		h.log.Errorw("resp.StatusCode", zap.Error(err))
+		h.Send(tgbotapi.NewMessage(message.Chat.ID, "Произошла ошибка"))
+		return nil, fmt.Errorf("resp.StatusCode: %w", err)
+	}
+
+	return resp, nil
+}
+
+func (h *Handler) requestFormal(ctx context.Context, message *tgbotapi.Message, resp *http.Response) error {
+	req, err := http.NewRequestWithContext(ctx, "POST", h.cfg.FormalURL+"/data", resp.Body)
+	if err != nil {
+		h.log.Errorw("http.NewRequest", zap.Error(err))
+		return fmt.Errorf("http.NewRequest: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		h.log.Errorw("client.Do", zap.Error(err))
+		return fmt.Errorf("client.Do: %w", err)
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			h.log.Errorw("resp.Body.Close", zap.Error(err))
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		h.log.Errorw("resp.StatusCode", zap.Error(err))
+		return fmt.Errorf("resp.StatusCode: %w", err)
+	}
+
+	var data domain.FormalResponse
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		h.log.Errorw("json.NewDecoder", zap.Error(err))
+		return fmt.Errorf("json.NewDecoder: %w", err)
+	}
+
+	var msg string
+	for _, result := range data.Result {
+		msg += result.Data + "\n"
+	}
+
+	h.Send(tgbotapi.NewMessage(message.Chat.ID, "The problem has been solved:\n"+msg))
+
+	return nil
+}
+
+func (h *Handler) Problem(ctx context.Context, message *tgbotapi.Message) error {
+	resp, err := h.requestParser(ctx, message)
+	if err != nil {
+		return err
+	}
+
+	err = h.requestFormal(ctx, message, resp)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (h *Handler) Theory(ctx context.Context) error {
