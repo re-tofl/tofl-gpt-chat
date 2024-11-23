996a8b52694516a339f288f0ba223a93f56cc6f7
Ivan Pavlov <darleet@mail.ru>
2024-11-23 17:09:14 +0300

    add parser errors output

--- internal/domain/parser.go
+++ internal/domain/parser.go
@@ -4,3 +4,8 @@ type ParserRequest struct {
 	TRS            string `json:"TRS"`
 	Interpretation string `json:"Interpretation"`
 }
+
+type ParserResponse struct {
+	ErrorTrs            []string `json:"error_trs"`
+	ErrorInterpretation []string `json:"error_interpretation"`
+}
