1470937376963f589ea139a88c7dfb3f409f77a0
Ivan Pavlov <darleet@mail.ru>
2024-12-15 19:27:47 +0300

    fix parser response schema

--- internal/domain/parser.go
+++ internal/domain/parser.go
@@ -10,21 +10,20 @@ type ParserErrorResponse struct {
 	ErrorInterpretation []string `json:"error_interpretation"`
 }
 
+type Child struct {
+	Value  string  `json:"value"`
+	Childs []Child `json:"childs"`
+}
+
 type ParserResponse struct {
 	JsonTRS []struct {
 		Left struct {
-			Value  string `json:"value"`
-			Childs []struct {
-				Value  string        `json:"value"`
-				Childs []interface{} `json:"childs"`
-			} `json:"childs"`
+			Value  string  `json:"value"`
+			Childs []Child `json:"childs"`
 		} `json:"left"`
 		Right struct {
-			Value  string `json:"value"`
-			Childs []struct {
-				Value  string        `json:"value"`
-				Childs []interface{} `json:"childs"`
-			} `json:"childs"`
+			Value  string  `json:"value"`
+			Childs []Child `json:"childs"`
 		} `json:"right"`
 	} `json:"json_TRS"`
 	JsonInterpret struct {
